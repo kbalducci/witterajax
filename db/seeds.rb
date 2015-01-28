c9801e8e6007da984b83ed4acd0c0567e895ad08
Kate <kbalducci@hotmail.com>
2015-01-27 22:08:46 -0600

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+wits = Wit.create([message: 'I love Ruby'])
+wits = Wit.create([message: 'Sometimes it works'])
